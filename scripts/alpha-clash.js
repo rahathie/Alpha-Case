eedec0d091410594a738dde048577db783c1d8b3
Rahat Hie <rahat.sparky07@gmail.com>
2024-02-17 00:49:26 +0000

    game function created in js file

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,30 @@
+//    function play(){
+//      const homeSection = document.getElementById('home-screen');
+//      homeSection.classList.add('hidden');
+//     console.log(homeSection.classList)
+
+//      const playgroundSection = document.getElementById('play-ground');
+//      playgroundSection.classList.remove('hidden');
+//         console.log(playgroundSection.classList)
+
+//  }
+
+function continueGame(){
+     const alphabet = getARandomAlphabet();
+     console.log('your random alphabet', alphabet);
+
+     const currentAlphabetElement = document.getElementById('current-alphabet');
+     currentAlphabetElement.innerText = alphabet;
+
+
+   setBackgroundColorById(alphabet);
+
+    }
+
+
+
 function play(){
-    console.log('play start now');
+    hideElementById('home-screen')
+    showElementById('play-ground');
+    continueGame()
 }
